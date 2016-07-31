49bacc4c613b8c01de08cf56ac3c6b4e98adacbb
Kevin Oh <aflashyrhetoric@gmail.com>
2016-07-31 11:27:06 -0400

    Implemented saved state thing. Removed popup files. Tweaked the way jQuery is loaded.

--- background.js
+++ background.js
@@ -1,39 +1,44 @@
-// function cookieExists() {
-//   "use strict"
-//   var cookieExists = chrome.cookies.get({
-//     'url': 'https://www.messenger.com/t/*',
-//     'name': 'nightMessengerToggle'
-//   });
-//   if(cookieExists == ''){
-//     return false;
-//   } else {
-//     return true;
-//   }
-// }
+"use strict";
+var toggle;
 
-// if(cookieExists()){
-//   alert('tits');
-//   var toggle = chrome.cookies.set({
-//     "url" : "https://www.messenger.com/t/*",
-//     "name": "nightMessengerToggle",
-//     "value": true
-//   });
-// } else {
-//   alert('boo');
-//   var toggle = false;
-// }
-var toggle = false;
-chrome.browserAction.onClicked.addListener(function (tab) {
-    toggle = !toggle;
-    if (toggle) {
-      // Change Icon
-      chrome.browserAction.setIcon({path: "on.png", tabId: tab.id});
-      // Apply Script + CSS
-      chrome.tabs.executeScript(tab.id, {file: "night.js"});
-    } else {
-      // Change Icon
-      chrome.browserAction.setIcon({path: "off.png", tabId: tab.id});
-      // Apply Script + CSS
-      chrome.tabs.executeScript(tab.id, {file: "day.js"});
-    }
+// Initialize toggle variable (on or off / true or false)
+chrome.storage.sync.get("activated", function(obj){
+  toggle = obj.activated;
+  console.log("toggle is " + obj.activated);
 });
+
+var initialCheck = function(o){
+  if (toggle) { 
+    // apply script + css
+    chrome.tabs.executeScript(o.tabId, { file: "night.js" });
+  } 
+}
+
+var toggleOnOff = function(tab){
+  toggle = !toggle;
+  if (toggle) { 
+    // Change Icon
+    chrome.browserAction.setIcon({path: "on.png", tabId: tab.id});
+    // Apply Script + CSS
+    chrome.tabs.executeScript(tab.id, {file: "night.js"});
+    chrome.storage.sync.set({ activated : true }, function(){
+      console.log("Activated");
+    });   
+  } else {
+    // Change Icon
+    chrome.browserAction.setIcon({path: "off.png", tabId: tab.id});
+    // Apply Script + CSS
+    chrome.tabs.executeScript(tab.id, {file: "day.js"});
+    chrome.storage.sync.set({ activated : false  }, function() {
+      console.log("Deactivated."); 
+    });
+  }
+};
+
+chrome.webNavigation.onCompleted.addListener(initialCheck, 
+  {
+    url: [
+      { hostContains: '.messenger'}
+    ]
+  });
+chrome.browserAction.onClicked.addListener(toggleOnOff);
--- night.js
+++ night.js
@@ -1,18 +1,21 @@
+"use strict";
 var a = chrome.extension.getURL("nightmode.css");
 
-// Removes the "Keep the conversation (...)" blurb
-if($('._s15').length){
-  $('._s15').detach();
-  window.dispatchEvent(new Event('resize'));
-}
+// setTimeout(function(){
+	// If jQuery not yet imported,
+	if(jQuery('#jquerylibrary').length){
+	  jQuery('<script src="jquery.js" id="jquerylibrary"</script>').appendTo("head");
+	}
 
-// If jQuery not yet imported,
-if($('#jquerylibrary').length){
-  $('<script src="jquery.js" id="jquerylibrary"</script>').appendTo("head");
-}
+	// Removes the "Keep the conversation (...)" blurb
+	if(jQuery('._s15').length){
+	  jQuery('._s15').detach();
+	  window.dispatchEvent(new Event('resize'));
+	}
 
-// If the stylesheet hasn't been applied, add it!
-if(!$('.nightMessenger').length){
-  $('<link rel="stylesheet" class="nightMessenger" type="text/css" href="' + a + '" >').appendTo("head");
-  console.log("woo");
-}
+	// If the stylesheet hasn't been applied, add it!
+	if(!jQuery('.nightMessenger').length){
+	  jQuery('<link rel="stylesheet" class="nightMessenger" type="text/css" href="' + a + '" >').appendTo("head");
+	}
+// }, 100)
+
